2e6852af00e6434b943f1013086c558c08b556ed
A0X0E0L <ax13xxi@gmail.com>
2023-01-09 16:07:26 +0000

    Contador de Segundos simple Traba. preliminar

---
+++ src/js/component/SecondsCounter.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SecondsCounter = (props) => {
+	return (
+	
+        <h1>Contador: {props.numero1}</h1>
+	);
+};
+
+export default SecondsCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,47 @@
 //import react into the bundle
-import React from "react";
+import React, {
+    Component
+} from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+import SecondsCounter from "./component/SecondsCounter.jsx";
+
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+let num4 = 0;
+let num5 = 0;
+let num6 = 0;
+
+setInterval(function() {
+            num1++;
+            //render your react application
+            ReactDOM.render( < SecondsCounter numero1 = {
+                    num1
+                }
+                numero2 = {
+                    num2
+                }
+                numero3 = {
+                    num3
+                }
+                numero4 = {
+                    num4
+                }
+                numero5 = {
+                    num5
+                }
+                numero6 = {
+                    num6
+                }
+                />, document.querySelector("#app"));
+            }, 1000);
+
+
+
+        //render your react application
